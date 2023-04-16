417d131f6b3795d5ccfa12754fe6a2f14709abc9
jju <totkfka4@gmail.com>
2023-04-16 20:45:02 +0900

    king

--- baldhead/lib/screens/view/loginpage/signuppage.dart
+++ baldhead/lib/screens/view/loginpage/signuppage.dart
@@ -1,4 +1,3 @@
-import 'package:baldhead/screens/home_screen/login_page/square_tile.dart';
 import 'package:flutter/material.dart';
 
 class SignUpPage extends StatelessWidget {
@@ -83,53 +82,6 @@ class SignUpPage extends StatelessWidget {
                     onPressed: signUserUp,
                     child: Text('Sign Up'),
                   ),
-
-                  const SizedBox(height: 50),
-
-                  // or continue with
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                    child: Row(
-                      children: [
-                        Expanded(
-                          child: Divider(
-                            thickness: 0.5,
-                            color: Colors.grey[400],
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 10.0),
-                          child: Text(
-                            'SNS 계정으로 가입하기',
-                            style: TextStyle(color: Colors.grey[700]),
-                          ),
-                        ),
-                        Expanded(
-                          child: Divider(
-                            thickness: 0.5,
-                            color: Colors.grey[400],
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-
-                  const SizedBox(height: 10),
-
-                  // google + apple sign in buttons
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: const [
-                      // google button
-                      SquareTile(imagePath: 'assets/images/kakao.png'),
-
-                      SizedBox(width: 25),
-
-                      // apple button
-                      SquareTile(imagePath: 'assets/images/google.png')
-                    ],
-                  ),
-                  const SizedBox(height: 10),
                 ],
               ),
             ),
