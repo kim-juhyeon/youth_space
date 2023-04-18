597c46c586036be6191ce609e17d94a8388d1a13
jju <totkfka4@gmail.com>
2023-04-18 19:01:55 +0900

    first Screen

--- baldhead/lib/screens/view/sharepage/mkdir_first_screen.dart
+++ baldhead/lib/screens/view/sharepage/mkdir_first_screen.dart
@@ -11,6 +11,8 @@ class FirstScreen extends StatefulWidget {
 class _FirstScreenState extends State<FirstScreen> {
   String _title = '';
   String _category = '책 읽기';
+  String _people = '5명 내외';
+  String _description = '';
 
   void _submit() {
     // Handle submission of new task
@@ -51,7 +53,7 @@ class _FirstScreenState extends State<FirstScreen> {
             padding: const EdgeInsets.all(16.0),
             child: TextFormField(
               decoration: InputDecoration(
-                hintText: 'Enter title',
+                hintText: '제목',
               ),
               onChanged: (value) {
                 setState(() {
@@ -85,8 +87,58 @@ class _FirstScreenState extends State<FirstScreen> {
                   value: '취미 공유',
                   child: Text('취미공유'),
                 ),
+                DropdownMenuItem(
+                  value: '맛집탐방',
+                  child: Text('맛집탐방'),
+                ),
               ],
-              icon: Icon(Icons.category),
+              icon: Icon(Icons.arrow_forward_ios),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: DropdownButtonFormField<String>(
+              decoration: InputDecoration(
+                hintText: '인원을 선택하세요',
+              ),
+              value: _people,
+              onChanged: (value) {
+                setState(() {
+                  _people = value!;
+                });
+              },
+              items: [
+                DropdownMenuItem(
+                  value: '5명 내외',
+                  child: Text('5명 내외'),
+                ),
+                DropdownMenuItem(
+                  value: '8명 내외',
+                  child: Text('8명 내외'),
+                ),
+                DropdownMenuItem(
+                  value: '15명 내외',
+                  child: Text('15명 내외'),
+                ),
+              ],
+              icon: Icon(Icons.arrow_forward_ios),
+            ),
+          ),
+          SizedBox(
+            height: 15,
+          ),
+          Padding(
+            padding:
+                const EdgeInsets.symmetric(vertical: 20.0, horizontal: 20.0),
+            child: TextFormField(
+              decoration: InputDecoration(
+                hintText: '설명',
+              ),
+              onChanged: (value) {
+                setState(() {
+                  _description = value;
+                });
+              },
             ),
           ),
         ],
