465cda4b5dc4e5ff183182ac7912e93f9bad576a
jju <totkfka4@gmail.com>
2023-04-25 20:25:20 +0900

    add

--- baldhead/lib/screens/view/sharepage/share_screen.dart
+++ baldhead/lib/screens/view/sharepage/share_screen.dart
@@ -1,10 +1,9 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'dialog_screen.dart';
 
 class Sharescreen extends StatefulWidget {
-  const Sharescreen({super.key});
+  const Sharescreen({Key? key}) : super(key: key);
 
   @override
   _SharescreenState createState() => _SharescreenState();
@@ -14,30 +13,38 @@ class _SharescreenState extends State<Sharescreen> {
   final CollectionReference _shareCollection =
       FirebaseFirestore.instance.collection('shares');
 
-  bool _showingDialog = false;
-
   void _showDialog() {
-    if (_showingDialog) {
-      return;
-    }
-    setState(() {
-      _showingDialog = true;
-    });
     showDialog(
       context: context,
       builder: (BuildContext context) {
         return Dialogcreat(onExitPressed: () {
-          setState(() {
-            _showingDialog = false;
-          });
           Navigator.of(context).pop();
         });
       },
-    ).then((_) {
-      setState(() {
-        _showingDialog = false;
-      });
-    });
+    );
+  }
+
+  Future<String> getUserName(String userId) async {
+    try {
+      print('Getting user for ID $userId');
+      DocumentSnapshot userSnapshot = await FirebaseFirestore.instance
+          .collection('users')
+          .doc(userId)
+          .get();
+      if (userSnapshot.exists) {
+        String name =
+            (userSnapshot.data() as Map<String, dynamic>)['name'] ?? 'gg';
+
+        print('Got name "$name" for user ID $userId');
+        return name;
+      } else {
+        throw Exception('User not found for ID $userId');
+      }
+    } catch (e, stackTrace) {
+      print('Error getting user name: $e');
+      print(stackTrace);
+      return 'name';
+    }
   }
 
   @override
@@ -48,10 +55,7 @@ class _SharescreenState extends State<Sharescreen> {
         backgroundColor: Colors.lightGreen[400],
       ),
       body: StreamBuilder<QuerySnapshot>(
-          stream: _shareCollection
-              .where('userId',
-                  isEqualTo: FirebaseAuth.instance.currentUser!.uid)
-              .snapshots(),
+          stream: _shareCollection.snapshots(),
           builder:
               (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
             if (!snapshot.hasData) {
@@ -70,25 +74,31 @@ class _SharescreenState extends State<Sharescreen> {
                       Text('Category: ${share['category']}'),
                       Text('People: ${share['people']}'),
                       Text('Description: ${share['description']}'),
+                      FutureBuilder<String>(
+                        future: share['userId'] != null
+                            ? getUserName(share['userId'])
+                            : Future.value(''),
+                        builder: (BuildContext context,
+                            AsyncSnapshot<String> snapshot) {
+                          if (snapshot.connectionState ==
+                              ConnectionState.waiting) {
+                            return CircularProgressIndicator();
+                          } else if (snapshot.hasError) {
+                            return Text('Error: ${snapshot.error}');
+                          } else {
+                            return Text('Name : ${snapshot.data}');
+                          }
+                        },
+                      )
                     ],
                   ),
                 );
               },
             );
           }),
-      floatingActionButton: Stack(
-        children: [
-          FloatingActionButton(
-            onPressed: () {
-              if (_showingDialog) {
-                Navigator.of(context).pop();
-              } else {
-                _showDialog();
-              }
-            },
-            child: _showingDialog ? Icon(Icons.close) : Icon(Icons.add),
-          ),
-        ],
+      floatingActionButton: FloatingActionButton(
+        onPressed: _showDialog,
+        child: Icon(Icons.add),
       ),
     );
   }
