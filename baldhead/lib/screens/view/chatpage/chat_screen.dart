5c83a9d5f712b31e11012ff35b72ede34727b862
jju <totkfka4@gmail.com>
2023-04-30 15:16:52 +0900

    chat first commit

--- baldhead/lib/screens/view/chatpage/chat_screen.dart
+++ baldhead/lib/screens/view/chatpage/chat_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
   final List<String> chatData;
   final Function(String) onChatAdded;
 
@@ -11,6 +11,13 @@ class ChatScreen extends StatelessWidget {
       required List<String> chatdata});
 
   @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  var _userEnterMessage = '';
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Chat')),
@@ -18,18 +25,39 @@ class ChatScreen extends StatelessWidget {
         children: [
           Expanded(
             child: ListView.builder(
-              itemCount: chatData.length,
+              itemCount: widget.chatData.length,
               itemBuilder: (context, index) {
-                final chat = chatData[index];
+                final chat = widget.chatData[index];
                 return ListTile(title: Text(chat));
               },
             ),
           ),
-          TextField(
-            onSubmitted: (value) {
-              onChatAdded(value);
-            },
-            decoration: InputDecoration(hintText: 'Type a message'),
+          Container(
+            padding: EdgeInsets.only(left: 16),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    onSubmitted: (value) {
+                      widget.onChatAdded(value);
+                    },
+                    decoration: InputDecoration(hintText: '메시지를 입력하세요.'),
+                    onChanged: (value) {
+                      setState(() {
+                        _userEnterMessage = value;
+                      });
+                    },
+                  ),
+                ),
+                Expanded(
+                  child: IconButton(
+                    onPressed: _userEnterMessage.trim().isEmpty ? null : () {},
+                    icon: Icon(Icons.send),
+                    color: Colors.blue,
+                  ),
+                )
+              ],
+            ),
           ),
         ],
       ),
